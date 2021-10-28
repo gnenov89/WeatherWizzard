355359acf93e1e856676c6ab345eb3cb108fbef8
Georgi <georgi.nenov89@gmail.com>
2021-10-28 16:24:31 -0400

    get city by name,state and zipcode

--- ui.js
+++ ui.js
@@ -1,5 +1,19 @@
 class UI {
     constructor(){
         this.location = document.getElementById('w-location');
+        this.desc = document.getElementById('w-desc'); 
+        this.string = document.getElementById('w-string');
+        this.details = document.getElementById('w-details');
+        this.icon = document.getElementById('w-icon');
+        this.humidity = document.getElementById('w-humidity');
+        this.dewpoint = document.getElementById('w-dewpoint');
+        this.wind = document.getElementById('w-wind');
+        this.feelsLike = document.getElementById('w-feels-like');
+    }
+
+    paint(weather) {
+        this.location.textContent = weather.current;
+        this.desc.textContent = weather.weather;
+
     }
 }
--- weather.js
+++ weather.js
@@ -11,7 +11,8 @@ class Weather {
     // Fetch weather from API 
     async getWeather() {
         // const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.state}}&appid=${this.apiKey}}`)
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${this.lat}&lon=${this.lon}&exclude=minutely,hourly,daily,alerts&units=metric&appid=${this.apiKey}`)
+        // const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${this.lat}&lon=${this.lon}&exclude=minutely,hourly,daily,alerts&units=metric&appid=${this.apiKey}`)
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Boston,us&zip=02116&appid=a151594aabbd859764c5db4204cd350d`);
 
         const responseData = await response.json();
         // console.log(responseData)
